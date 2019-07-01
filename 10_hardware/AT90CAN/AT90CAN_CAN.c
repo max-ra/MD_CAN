98a82395cfaf86bd86e8eee054da3d7947f9cc40
Maximilian Rauch <maximilian_rauch@yahoo.de>
2019-07-02 00:40:39 +0200

    hardware description for AT90CAN

---
+++ 10_hardware/AT90CAN/AT90CAN_CAN.c
@@ -0,0 +1,23 @@
+/*
+ * AT90CAN_CAN.c
+ *
+ * Created: 30.06.2019 22:49:05
+ *  Author: maxi
+ */ 
+
+// ##### Including #####
+
+
+// ##### Settings #####
+
+
+// ##### definition #####
+
+
+// ##### Status definition #####
+
+
+// ##### variable #####
+
+
+// ##### function #####
